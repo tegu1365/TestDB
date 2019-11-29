1fc924bab4d7029667fe1bd17b4d4bd1f10cb4d6
tegu1365 <tedi.d.petkova@gmail.com>
2019-11-29 18:41:49 +0200

    Added Services for File and Profile

---
+++ Services/FileService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestDatabase.Models;
+using System.Linq;
+
+namespace TestDatabase.Services
+{
+    class FileService
+    {
+        TestContext _context;
+
+        public FileService(TestContext context)
+        {
+            _context = context;
+        }
+
+        public void AddFile(string p)
+        {
+            File file = new File();
+            file.Path = p;
+            _context.Files.Add(file);
+            _context.SaveChanges();
+        }
+
+        public File GetFile(string p)
+        {
+            return _context.Files.First(x => x.Path == p);
+        }
+
+        public void DeleteFile(string p)
+        {
+            _context.Files.Remove(GetFile(p));
+            _context.SaveChanges();
+        }
+
+        public void ChangePath(string oldP, string newP)
+        {
+            File file = GetFile(oldP);
+            file.Path = newP;
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ Services/ProfileService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestDatabase.Models;
+
+namespace TestDatabase.Services
+{
+    class ProfileService
+    {
+        private TestContext _context;
+
+        public ProfileService (TestContext context)
+        {
+           _context = context;
+        }
+
+        public void AddProfile(string n, string e, string p, ProfileType pt)
+        {
+            Profile person = new Profile();
+
+            person.Name = n;
+            person.Email = e;
+            person.Password = p;
+            person.ProfileType = pt;
+
+            _context.Profiles.Add(person);
+            _context.SaveChanges();
+        }
+
+        public Profile GetProfile(string e, string p)
+        {
+            return _context.Profiles.First(x => x.Email == e && x.Password == p);
+        }
+
+        public void DeleteProfile(string e, string p)
+        {
+            _context.Profiles.Remove(GetProfile(e, p));
+            _context.SaveChanges();
+        }
+
+        public void ReNameProfil(string e, string p, string newName)
+        {
+            Profile person = GetProfile(e, p);
+            person.Name = newName;
+            _context.SaveChanges();
+        }
+
+        public void ChangeProfileType(string e, string p, ProfileType newPT)
+        {
+            Profile person = GetProfile(e, p);
+            person.ProfileType = newPT;
+            _context.SaveChanges();
+        }
+    }
+}
